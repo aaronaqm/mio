f6d4ddcc229e9708e7ecc17a725c89e1aadf2c83
aaronaqm <aaronaquinomanyari@gmail.com>
2017-07-19 22:50:33 -0700

    fin de la noche
    ya me duermo subo lo que hice

---
+++ footer.php
@@ -0,0 +1,67 @@
+<link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+<!--footer start from here-->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-sm-6 footer-col">
+        <div class="logofooter"> <img src="imgx/logobajo.png" width="220" height="49" /></div>
+        <p>Desde el año 1997, RTS Automation S.A.C. ha desarrollado importantes proyectos de automatización y sistemas eléctricos   generando y acumulando dentro de la organización una especialización, experiencia y calidad que ofrecemos a nuestros clientes y proveedores.</p>
+        <p><i class="fa fa-map-pin"></i> Jr. Huaraz 1882 - Breña -Lima - Peru</p>
+        <p><i class="fa fa-phone"></i> Phone (51) : 01 628 0090</p>
+        <p><i class="fa fa-envelope"></i> E-mail : ventas@rts-auto.com</p>
+        <p><i class="fa fa-envelope"></i> E-mail : servicios@rts-auto.com</p>
+      </div>
+      <div class="col-md-3 col-sm-6 footer-col">
+        <h6 class="heading7">ENLACES</h6>
+        <ul class="footer-ul">
+          <li><a href="#"> Brochure general</a></li>
+          <li><a href="#"> Contactenos</a></li>
+           <li><a href="#">Mapa</a></li>
+          <li><a href="#"> Nosotros</a></li>
+          <li><a href="#"> Cotizaciones</a></li>
+          <li><a href="#"> Trabaje con nosotros</a></li>
+
+        </ul>
+      </div>
+      <div class="col-md-3 col-sm-6 footer-col">
+        <h6 class="heading7">Novedades</h6>
+        <div class="post">
+          <p>RTS Automation participará en EXPOMINA 2017, ciudad de Arequipa <span>Julio 14,2017</span></p>
+          <p>Novedades equipos VEGA <span>Julio 1,2017</span></p>
+          <p>Nuevo video detector de metal Tectron<span>Enero 2,2017</span></p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 footer-col">
+        <h6 class="heading7">Social Media</h6>
+        <ul class="footer-social">
+          <li><i class="fa fa-linkedin social-icon linked-in" aria-hidden="true"></i></li>
+          <li><i class="fa fa-facebook social-icon facebook" aria-hidden="true"></i></li>
+          <li><i class="fa fa-youtube social-icon youtube" aria-hidden="true"></i></li>
+          <li><i class="fa fa-google-plus social-icon google" aria-hidden="true"></i></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--footer start from here-->
+<!--pie de pagina tomado de  prabuuideveloper-->
+<div class="copyright">
+  <div class="container">
+    <div class="col-md-6">
+
+      <p>Julio 2017</p>
+    </div>
+    <div class="col-md-6">
+      <ul class="bottom_ul">
+
+        <li><a href="#">Acerca de nosotros</a></li>
+        <li><a href="#">Faq's</a></li>
+        <li><a href="#">Contactenos</a></li>
+        <li><a href="#">Site Map</a></li>
+        <li><a href="#">Desarrollado por Aaron Aquino Manyari</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
